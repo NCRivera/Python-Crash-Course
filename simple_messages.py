9038ecd7bee043ca4b9e1cb53b523a6a1f7d7d36
NCRivera <41407150+NCRivera@users.noreply.github.com>
2018-08-25 18:59:46 -0400

    Delete simple_messages.py

--- simple_messages.py
+++
@@ -1,5 +0,0 @@
-simple_message = "Once again this is another simple task that I'm completing for exercise 2-2."
-print(simple_message)
-
-simple_message = "Here is the second message."
-print(simple_message)
